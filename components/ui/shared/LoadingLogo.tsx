0a61a6bd6c004fe600a8a5ed549ed1e27e9bfc5c
StellarIV <biruktmg@gmail.com>
2024-08-02 18:27:34 +0300

    Forth commit

--- components/ui/shared/LoadingLogo.tsx
+++ components/ui/shared/LoadingLogo.tsx
@@ -1,22 +1,21 @@
-import Image from "next/image"
+import Image from "next/image";
 
 type Props = {
-    size?: Number;
-
+    size?: number; // Change Number to number
 };
 
 const LoadingLogo = ({ size = 100 }: Props) => {
-  return ( <div className="h-full w-full flex justify-center items-center">
-    <Image 
-    src="/logo.svg" 
-    alt="logo" 
-    width={size} 
-    height={size} 
-    className="animate-pulse duration-800"
-    />
-  </div>
-    
-  )
-}
+    return (
+        <div className="h-full w-full flex justify-center items-center">
+            <Image
+                src="/logo.svg"
+                alt="logo"
+                width={size}
+                height={size}
+                className="animate-pulse duration-800"
+            />
+        </div>
+    );
+};
 
-export default LoadingLogo
+export default LoadingLogo;
